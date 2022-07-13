90f7a6698e74ac7a6b6fcb5e85c8127d707026af
sky <690051852@qq.com>
2022-07-13 15:06:37 +0800

    修改测试用例

--- src/test/java/test/Demo.java
+++ src/test/java/test/Demo.java
@@ -7,13 +7,15 @@ import org.schema.verify.Verify;
 public class Demo {
 
     public static void main(String[] args) {
-        String json = "{\"name\": [{\"age\": 1}, {\"age\": 3}, {\"age\": 2}]}";
+        String json = "{\"name\": [{\"age\": 2}, {\"age\": 3}, {\"age\": 2}], \"age\": 18}";
         Schema schema = JSON.object()
                 .attr("name", JSON.array()
                         .item(
                                 JSON.object().attr("age", JSON.number().enumVal(2, 3))
                         )
-                ).require("name");
+                )
+                .attr("age", JSON.number().max(20))
+                .require("name", "age");
 
         System.out.println(Verify.verify(json, schema));
 
