1bd8d1e33340511a7fa3423f1356df6a6e6925b4
sky <690051852@qq.com>
2022-07-14 13:05:41 +0800

    修复bug

--- src/main/java/org/schema/verify/impl/VerifyImpl.java
+++ src/main/java/org/schema/verify/impl/VerifyImpl.java
@@ -146,19 +146,19 @@ public class VerifyImpl {
 
                 //防止空指针异常
                 if (Objects.nonNull(parseData)){
-                    //如果是String类型(直接传,不需要做json转换)
-                    if (entry.getValue().getClass().equals(StringSchema.class)){
+                    //如果是Object或Arr类型(要做json转换)
+                    if (entry.getValue().getClass().equals(ObjectSchema.class) || entry.getValue().getClass().equals(ArraySchema.class)){
+                        //拿到key值(递归继续查找)(如果是错的就返回)
+                        if(!verifySchema(this.objectMapper.writeValueAsString(parseData), entry.getValue())){
+                            throw new RuntimeException(this.errorMessage);
+                        }
+                    }else {
                         //拿到key值(递归继续查找)(如果是错的就返回)
                         if(!verifySchema(parseData, entry.getValue())){
                             throw new RuntimeException(this.errorMessage);
                         }
-                        //跳过
-                        continue;
                     }
-                    //拿到key值(递归继续查找)(如果是错的就返回)
-                    if(!verifySchema(this.objectMapper.writeValueAsString(parseData), entry.getValue())){
-                        throw new RuntimeException(this.errorMessage);
-                    }
+
                 }
 
             }
